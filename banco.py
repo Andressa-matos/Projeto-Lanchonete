649a4976afd6e3071971b3ba4ec7ced3ab507448
Andressa  Matos <94369142+Andressa-matos@users.noreply.github.com>
2023-06-04 11:32:10 -0300

    Create banco.py

---
+++ banco.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("UserData.db")
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Users(
+    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL,
+    Usuario TEXT NOT NULL,
+    Password TEXT NOT NULL
+);
+""")
+
+# cadastroUsuario()
