ead051b5303e784ba104972ae9f11ad8f44c70d4
Andressa  Matos <94369142+Andressa-matos@users.noreply.github.com>
2023-06-19 08:44:17 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -2,9 +2,10 @@ import sqlite3
 
 conn = sqlite3.connect("UserData.db")
 cursor = conn.cursor()
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Users(
-    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
     Name TEXT NOT NULL,
     Usuario TEXT NOT NULL,
     Password TEXT NOT NULL
